b0f99ce37bd23b8a30c0586b338c86a401bf5577
Juraj Uhlar <juro.uhlar@gmail.com>
2019-12-09 16:58:44 +0000

    Basic move and jump

--- scripts/Actor.gd
+++ scripts/Actor.gd
@@ -2,14 +2,11 @@ extends KinematicBody2D
 class_name Actor
 
 
-export var speed: = Vector2(300.0, 1000.0)
-export var gravity: = 200.0
+const FLOOR_NORMAL = Vector2.UP
+
+export var speed: = Vector2(200.0, 500.0)
+export var gravity: = 2000.0
 
 
 var velocity: = Vector2.ZERO
 
-
-func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity)
-	
