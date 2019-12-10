1a06c4de1a16bfa55b53e4e1780a5fe63215a8d9
Juraj Uhlar <juro.uhlar@gmail.com>
2019-12-10 02:52:34 +0000

    Grounded jump tolerance

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,17 +1,32 @@
 extends Actor
 
+onready var grounded_tolerance_timer = get_node("grounded_tolerance_timer")
+var can_jump = true
+var grounded_last_frame;
+
 func _physics_process(delta):
-	
-	
 	var direction = get_direction()
-	velocity = calculcate_move_velocity(velocity, direction, speed)
-	velocity = move_and_slide(velocity, FLOOR_NORMAL)
-	print (is_on_floor())
+	_velocity = calculcate_move_velocity(_velocity, direction, speed)
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+	
+	
+	if Input.is_action_just_pressed("jump"):
+		can_jump = false;
+
+	if (is_on_floor() == false) and (grounded_last_frame == true) :
+		grounded_tolerance_timer.start()
+		
+	if (is_on_floor()):
+		grounded_last_frame = true
+		can_jump = true;
+	else: 
+		grounded_last_frame = false
+		
 	
 func get_direction():
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
+		-1.0 if Input.is_action_just_pressed("jump") and is_grounded() and can_jump else 1.0
 	)
 	
 func calculcate_move_velocity(
@@ -25,7 +40,9 @@ func calculcate_move_velocity(
 	if direction.y == -1.0:
 		new_velocity.y = speed.y * direction.y
 	return new_velocity
+	
+func is_grounded():
+	return is_on_floor() or (grounded_tolerance_timer.time_left > 0 and _velocity.y > 0)
 
-
-
-
+func _on_grounded_tolerance_timer_timeout():
+	can_jump = false
