a5183103bf7723b6c9ee629cbf729a4687a60fac
Juraj Uhlar <juro.uhlar@gmail.com>
2019-12-10 22:14:32 +0000

    Basic horizontal dash

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,37 +4,49 @@ onready var grounded_tolerance_timer = get_node("grounded_tolerance_timer")
 onready var jump_trigger_tolerance_timer = get_node("jump_trigger_tolerance_timer")
 
 var can_jump = true
-var grounded_last_frame;
+var grounded_last_frame
+var dashing = false
 
 func _physics_process(delta):
 	
-	var direction = get_direction()
-	_velocity = calculcate_move_velocity(_velocity, direction, speed)
-	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+	if(dashing):
+		var direction = Vector2(1.0,0.0) if ($Sprite.flip_h == false) else Vector2(-1.0,0)
+		_velocity = move_and_slide(Vector2(direction.x * speed.x * 2,0), FLOOR_NORMAL)
+	else:
 	
-	if Input.is_action_pressed("move_left"):
-		$Sprite.flip_h = true
-	if Input.is_action_pressed("move_right"):
-		$Sprite.flip_h = false
-
-	# Grounded jump tolearance 
-	if Input.is_action_just_pressed("jump"):
-		can_jump = false;
-		jump_trigger_tolerance_timer.start()
-
-	if (is_on_floor() == false) and (grounded_last_frame == true) :
-		grounded_tolerance_timer.start()
+		var direction = get_direction()
+		_velocity = calculcate_move_velocity(_velocity, direction, speed)
+		_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
 		
-	if (is_on_floor()):
-		grounded_last_frame = true
-		can_jump = true;
-	else: 
-		grounded_last_frame = false
+		if Input.is_action_pressed("move_left"):
+			$Sprite.flip_h = true
+		if Input.is_action_pressed("move_right"):
+			$Sprite.flip_h = false
+			
+		if Input.is_action_just_pressed("dash") and $dash_cooldown.time_left <= 0:
+			dashing = true
+			$dash_timer.start()
+			
+		
 	
-		# Prevents unintetional double jump when hittin a low ceiling 	
-	if is_on_ceiling():
-		jump_trigger_tolerance_timer.stop()
-	# /Grounded jump tolerance	
+		# Grounded jump tolearance 
+		if Input.is_action_just_pressed("jump"):
+			can_jump = false;
+			jump_trigger_tolerance_timer.start()
+	
+		if (is_on_floor() == false) and (grounded_last_frame == true) :
+			grounded_tolerance_timer.start()
+			
+		if (is_on_floor()):
+			grounded_last_frame = true
+			can_jump = true;
+		else: 
+			grounded_last_frame = false
+		
+			# Prevents unintetional double jump when hittin a low ceiling 	
+		if is_on_ceiling():
+			jump_trigger_tolerance_timer.stop()
+		# /Grounded jump tolerance	
 	
 func get_direction():
 	return Vector2(
@@ -59,3 +71,9 @@ func is_grounded():
 	
 func jumped():
 	return jump_trigger_tolerance_timer.time_left > 0 or Input.is_action_just_pressed("jump")
+
+
+func _on_dash_timer_timeout():
+	dashing = false
+	$dash_cooldown.start()
+
