8fab7b5fd6ef243c96534a2a1c4c2020f2c6f027
Juraj Uhlar <juro.uhlar@gmail.com>
2019-12-27 20:38:11 +0100

    Bullets kill enemy

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -1,23 +1,43 @@
 extends Actor
 
 export var speed = Vector2(100,200)
+export var hp = 30
 
+var dying = false
 
 func _ready():
 	_velocity.x = -speed.x
 	set_physics_process(false)
 	add_to_group("enemies")
+	add_to_group("damageable")
 
 func _physics_process(delta):
-	_velocity.y += gravity * delta
-	if is_on_wall():
-		_velocity.x *= -1.0
-	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
-	
-	if sign(_velocity.x) == 1:
-		$Sprite.flip_h = true
-	else: 
-		$Sprite.flip_h = false
+	if(dying):
+		pass
+	else:
+		_velocity.y += gravity * delta
+		if is_on_wall():
+			_velocity.x *= -1.0
+		_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
+		
+		if sign(_velocity.x) == 1:
+			$Sprite.flip_h = true
+		else: 
+			$Sprite.flip_h = false
+		
+func take_damage(damage):
+	hp -= damage
+	if hp <= 0:
+		die()
+		
+func die():
+	dying = true
+	$CollisionShape2D.disabled = true
+	$CollisionShape2D.scale = Vector2.ZERO
+	remove_from_group("enemies")
+	$Sprite.play("die")
+	yield($Sprite, "animation_finished")
+
 
 
 
--- scripts/player_bullet.gd
+++ scripts/player_bullet.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 export var speed = 350
+export var damage = 10
 var velocity = Vector2()
 var direction = 1
 var hit = false
@@ -23,7 +24,12 @@ func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
 
 func _on_player_bullet_body_entered(body):
+	print("Bullet Collision")
+	$CollisionShape2D.disabled = true
+	if(body.is_in_group("damageable")):
+		body.take_damage(damage)
 	$Sprite.play("impact")
 	hit = true
+	
 	yield($Sprite, "animation_finished")
 	queue_free()
