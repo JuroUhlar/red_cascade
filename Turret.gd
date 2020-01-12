590db0f8c9d76c59756db56d38da62f7bb1c70d4
Juraj Uhlar <juro.uhlar@gmail.com>
2020-01-12 21:11:03 +0000

    Turrets can die

--- Turret.gd
+++ Turret.gd
@@ -2,8 +2,14 @@ extends StaticBody2D
 
 export var deployed = false
 export var sensor_range = 400
+export var hp = 50
 export (PackedScene) var bullet
 
+var dead = false
+
+func _ready():
+	add_to_group("turrets")
+	add_to_group("damageable")
 
 func _physics_process(delta):
 	if ($RayCast_left.is_colliding() or $RayCast_right.is_colliding()):
@@ -14,7 +20,7 @@ func _physics_process(delta):
 			$RayCast_right.cast_to = Vector2(sensor_range, 0)
 			deployed = true
 		
-		if deployed:
+		if deployed and !dead:
 			if $RayCast_left.is_colliding():
 				$Sprite.flip_h = true
 				if sign($muzzle_position.position.x) == 1:
@@ -27,7 +33,9 @@ func _physics_process(delta):
 			if $gun_timer.time_left <= 0: 
 				shoot()
 		
-	if deployed: $Sprite.play("deploy")
+
+	if dead: $Sprite.play("die")
+	elif deployed: $Sprite.play("deploy")
 	
 func shoot():
 	$gun_timer.start()
@@ -38,4 +46,15 @@ func shoot():
 		projectile.set_projectile_direction(-1)
 	get_parent().add_child(projectile)
 	projectile.position = $muzzle_position.global_position
+	
+func take_damage(damage):
+	hp -= damage
+	if hp <= 0:
+		die()
+		
+func die():
+	$CollisionShape2D.disabled = true
+	$CollisionShape2D.scale = Vector2.ZERO
+	dead = true
+	
 
