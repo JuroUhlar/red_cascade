b2af894f95bb38f87bab3a8ab8b5b47bcb1052a2
Juraj Uhlar <juro.uhlar@gmail.com>
2020-01-13 00:44:30 +0000

    Dash upgrade info dialogue

--- scripts/Dash_upgrade.gd
+++ scripts/Dash_upgrade.gd
@@ -1,11 +1,19 @@
 extends Area2D
 
+export (NodePath) var dialog_path
+var dialog_node
 
+func _ready():
+	if (dialog_path.is_empty()):
+		print("Dialog path empty")
+	else:
+		dialog_node = get_node(dialog_path)
 
 func _on_Dash_upgrade_body_entered(body):
 	if(body.is_in_group("player")):
 		body.get_dash()
 		$AnimationPlayer.play("picked_up")
 		yield($AnimationPlayer, "animation_finished")
+		dialog_node.activate()
 		queue_free()
 
